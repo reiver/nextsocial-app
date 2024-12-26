facf76576905cc934d68632c75bfdea4e2272c89
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-26 15:25:15 -0800

    made separate pages for each tool-bar buton; still a skeleton app

--- main.go
+++ main.go
@@ -4,12 +4,7 @@ import (
 	"fmt"
 
 	"fyne.io/fyne/v2"
-	fyneapp       "fyne.io/fyne/v2/app"
-	fynecontainer "fyne.io/fyne/v2/container"
-	fynetheme     "fyne.io/fyne/v2/theme"
-	fynewidget    "fyne.io/fyne/v2/widget"
-
-	"github.com/reiver/nextsocial-app/icons"
+	fyneapp "fyne.io/fyne/v2/app"
 )
 
 func main() {
@@ -20,38 +15,10 @@ func main() {
 	var window fyne.Window = app.NewWindow("NextSocial")
 	window.Resize(fyne.NewSize(640,480))
 
-	var content *fyne.Container
-	{
-		var label *fynewidget.Label = fynewidget.NewLabel("NextSocial 💬")
+	var ui UI
+	ui.Init()
 
-		var toolbar *fynewidget.Toolbar = fynewidget.NewToolbar(
-			fynewidget.NewToolbarAction(icons.Account(), func(){
-				fmt.Println("account-button activated")
-			}),
-			fynewidget.NewToolbarSpacer(),
-			fynewidget.NewToolbarAction(icons.Search(), func(){
-				fmt.Println("search-button activated")
-			}),
-			fynewidget.NewToolbarSpacer(),
-			fynewidget.NewToolbarAction(icons.Menu(), func(){
-				fmt.Println("menu-button activated")
-			}),
-			fynewidget.NewToolbarSpacer(),
-			fynewidget.NewToolbarAction(icons.Notifications(), func(){
-				fmt.Println("notifications-button activated")
-			}),
-			fynewidget.NewToolbarSpacer(),
-			fynewidget.NewToolbarAction(icons.Home(), func(){
-				fmt.Println("home-button activated")
-			}),
-			fynewidget.NewToolbarSeparator(),
-			fynewidget.NewToolbarAction(fynetheme.DocumentCreateIcon(), func() {
-				fmt.Println("New document")
-			}),
-		)
-
-		content = fynecontainer.NewBorder(nil, toolbar, nil, nil, label)
-	}
+	var content fyne.CanvasObject = ui.Content()
 
 	window.SetContent(content)
 	window.ShowAndRun()
---
+++ ui.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
+	fynecontainer "fyne.io/fyne/v2/container"
+	fynetheme     "fyne.io/fyne/v2/theme"
+	fynewidget    "fyne.io/fyne/v2/widget"
+
+	"github.com/reiver/nextsocial-app/icons"
+)
+
+type UI struct {
+	container *fyne.Container
+	pageAccount       fyne.CanvasObject
+	pageHome          fyne.CanvasObject
+	pageMenu          fyne.CanvasObject
+	pageNew           fyne.CanvasObject
+	pageNotifications fyne.CanvasObject
+	pageSearch        fyne.CanvasObject
+}
+
+func (receiver *UI) Content() fyne.CanvasObject {
+	return receiver.container
+}
+
+func (receiver *UI) Init() {
+	if nil == receiver {
+		return
+	}
+
+	receiver.pageAccount       = fynewidget.NewLabel("NextSocial 💬 Account")
+	receiver.pageHome          = fynewidget.NewLabel("NextSocial 💬 Home")
+	receiver.pageMenu          = fynewidget.NewLabel("NextSocial 💬 Menu")
+	receiver.pageNew           = fynewidget.NewLabel("NextSocial 💬 New")
+	receiver.pageNotifications = fynewidget.NewLabel("NextSocial 💬 Notifications")
+	receiver.pageSearch        = func() fyne.CanvasObject {
+		var input *fynewidget.Entry = fynewidget.NewEntry()
+		input.SetPlaceHolder("search...")
+		input.OnSubmitted = func(str string){
+			fmt.Println("searching:", input.Text)
+		}
+
+		receiver.ShowPageSearch()
+
+		return input
+	}()
+
+	var content *fyne.Container
+	{
+		var label *fynewidget.Label = fynewidget.NewLabel("NextSocial 💬")
+
+		var toolbar *fynewidget.Toolbar = fynewidget.NewToolbar(
+			fynewidget.NewToolbarAction(icons.Account(), receiver.ShowPageAccount),
+			fynewidget.NewToolbarSpacer(),
+			fynewidget.NewToolbarAction(icons.Search(), receiver.ShowPageSearch),
+			fynewidget.NewToolbarSpacer(),
+			fynewidget.NewToolbarAction(icons.Menu(), receiver.ShowPageMenu),
+			fynewidget.NewToolbarSpacer(),
+			fynewidget.NewToolbarAction(icons.Notifications(), receiver.ShowPageNotifications),
+			fynewidget.NewToolbarSpacer(),
+			fynewidget.NewToolbarAction(icons.Home(), receiver.ShowPageHome),
+			fynewidget.NewToolbarSeparator(),
+			fynewidget.NewToolbarAction(fynetheme.DocumentCreateIcon(), receiver.ShowPageNew),
+		)
+
+
+		content = fynecontainer.NewBorder(nil, toolbar, nil, nil, label)
+	}
+
+	receiver.container = content
+}
+
+func (receiver *UI) show(canvasObject fyne.CanvasObject) {
+	if nil == receiver {
+		return
+	}
+
+	if nil == canvasObject {
+		return
+	}
+
+	container := receiver.container
+	if nil == container {
+		return
+	}
+
+	if len(container.Objects) <= 0 {
+		return
+	}
+
+	container.Objects[0] = canvasObject
+}
+
+func (receiver *UI) ShowPageAccount() {
+	fmt.Println("account-button activated")
+	receiver.show(receiver.pageAccount)
+}
+
+func (receiver *UI) ShowPageHome() {
+	fmt.Println("home-button activated")
+	receiver.show(receiver.pageHome)
+}
+
+func (receiver *UI) ShowPageMenu() {
+	fmt.Println("menu-button activated")
+	receiver.show(receiver.pageMenu)
+}
+
+func (receiver *UI) ShowPageNew() {
+	fmt.Println("new-button activated")
+	receiver.show(receiver.pageNew)
+}
+
+func (receiver *UI) ShowPageNotifications() {
+	fmt.Println("notifications-button activated")
+	receiver.show(receiver.pageNotifications)
+}
+
+func (receiver *UI) ShowPageSearch() {
+	fmt.Println("search-button activated")
+	receiver.show(receiver.pageSearch)
+}
